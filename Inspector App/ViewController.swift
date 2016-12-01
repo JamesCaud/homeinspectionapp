93adc440cabddd9ed350be4e74f638d9d63b6281
Jared Speck <jspeck@csc232x22.ad.calpoly.edu>
2016-11-30 16:38:02 -0800

    Clean Start?

---
+++ Inspector App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inspector App
+//
+//  Created by Jared Speck on 11/30/16.
+//  Copyright © 2016 Jared Speck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
